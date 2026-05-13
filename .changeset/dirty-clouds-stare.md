668b9014aee9187bcd804a832df01f89d0f73afc
Michael Trang <mtrang@woolworths.com.au>
2026-05-13 20:48:01 +1000

    Fix package exports to use correct .mjs/.d.mts extensions

---
+++ .changeset/dirty-clouds-stare.md
@@ -0,0 +1,5 @@
+---
+"@mtrangio/ui": patch
+---
+
+Fix package exports to use correct .mjs/.d.mts extensions
--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
 import "@mtrangio/ui/globals.css"
+import "./styles/app.css"
 import { App } from "./App.tsx"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
 
